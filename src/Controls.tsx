d274438fba0ac994989a13e0e460e36f31cd2839
Julian Quenet <julianquenet8@gmail.com>
2023-08-09 15:36:04 +0200

    Switched to canon, donig reworks

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,142 +1,25 @@
-import React, { useRef, useState } from "react";
-import { useFrame } from "@react-three/fiber";
-import { PointerLockControls, Box, PerspectiveCamera } from "@react-three/drei";
-import { RigidBody } from "@react-three/rapier";
-import { Raycaster, Vector3 } from "three";
+import { useState, useEffect } from 'react';
 
-const Controls = () => {
-  const controlsRef = useRef<any>();
-  const bodyRef = useRef<any>();
-  
-  const targetRef = useRef<any>();
-  const isLocked = useRef(false);
-  const [moveForward, setMoveForward] = useState(false);
-  const [moveBackward, setMoveBackward] = useState(false);
-  const [moveLeft, setMoveLeft] = useState(false);
-  const [moveRight, setMoveRight] = useState(false);
-  const [wall, setWall] = useState(false)
+ const usePlayerControls = () => {
+  const keys:any = { KeyW: 'forward', KeyS: 'backward', KeyA: 'left', KeyD: 'right', Space: 'jump' };
+  const moveFieldByKey = (key:any) => keys[key];
 
-  const rayCast = new Raycaster
-  useFrame(() => {
-    const velocity = 0.05;
-    let x = controlsRef.current.camera.position.x
-    let z = controlsRef.current.camera.position.z
+  const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false });
 
-    rayCast.setFromCamera(controlsRef.current.camera.position, controlsRef.current.camera);
-    
-    const hitWall = rayCast.intersectObject(targetRef.current)
-    if(hitWall.length > 0 ){
-      controlsRef.current.camera.position.x = x*0.1
-      controlsRef.current.camera.position.z = z*0.1
-    }
-    
-    if (moveForward) {
-      controlsRef.current.moveForward(velocity);
-    } else if (moveLeft) {
-      controlsRef.current.moveRight(-velocity);
-    } else if (moveBackward) {
-      controlsRef.current.moveForward(-velocity);
-      
-    } else if (moveRight) {
-      controlsRef.current.moveRight(velocity);
-      
-    }
-    bodyRef.current.setTranslation({x:x,y:1.2,z:z},true)
-  });
-   
-  
-  const onKeyDown =  (event:any) =>{
-    
-    switch (event.code) {
-      case "ArrowUp":
-      case "KeyW":
-        setMoveForward(true);
-        break;
+  useEffect(() => {
+    const handleKeyDown = (e:any) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }));
+    const handleKeyUp = (e:any) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }));
 
-      case "ArrowLeft":
-      case "KeyA":
-        setMoveLeft(true);
-        
-        break;
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
 
-      case "ArrowDown":
-      case "KeyS":
-        setMoveBackward(true);
-        
-        break;
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
 
-      case "ArrowRight":
-      case "KeyD":
-        setMoveRight(true);
-        
-        break;
-      default:
-        return;
-    }
-  };
-
-  const onKeyUp = function (event:any) {
-    switch (event.code) {
-      case "ArrowUp":
-      case "KeyW":
-        setMoveForward(false);
-        break;
-
-      case "ArrowLeft":
-      case "KeyA":
-        setMoveLeft(false);
-        break;
-
-      case "ArrowDown":
-      case "KeyS":
-        setMoveBackward(false);
-        break;
-
-      case "ArrowRight":
-      case "KeyD":
-        setMoveRight(false);
-        break;
-
-      default:
-        return;
-    }
-  };
-
-  window.addEventListener("keydown", onKeyDown);
-  window.addEventListener("keyup", onKeyUp);
-
-  return (
-    <>
-        <PointerLockControls
-      onUpdate={() => {
-        if (controlsRef.current) {
-          controlsRef.current.addEventListener("lock", () => {
-            isLocked.current = true;
-          });
-          controlsRef.current.addEventListener("unlock", () => {
-           
-            isLocked.current = false;
-          });
-        }
-      }}
-      ref={controlsRef}
-    />
-    
-    <RigidBody type="dynamic" ref={bodyRef} colliders={"ball"}>
-      <Box castShadow >
-        <meshStandardMaterial color="green"/>
-      </Box>
-    </RigidBody>
-
-    
-
-    <RigidBody type="fixed">
-        <Box castShadow position={[0, 1, 0]} args={[5,5,10]} ref={targetRef}>
-          <meshStandardMaterial color="darkslategrey" />
-        </Box>
-      </RigidBody>
-   </> 
-  );
+  return movement;
 };
 
-export default Controls;
+export default usePlayerControls
--- src/scene.tsx
+++ src/scene.tsx
@@ -1,34 +1,50 @@
-import {
-  Box,
-  PerspectiveCamera
-} from "@react-three/drei";
+import { Canvas } from '@react-three/fiber';
+import { Loader, PointerLockControls } from '@react-three/drei';
+import { Physics, usePlane } from '@react-three/cannon';
 
-import { RigidBody } from "@react-three/rapier";
+const Floor = (props:any) => {
+  const [ref]:any = usePlane((index) => ({ type: 'Static', mass: 0, ...props }));
+
+  return (
+    <mesh receiveShadow rotation={props.rotation} ref={ref}>
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial color={props.color} />
+    </mesh>
+  );
+};
 
 
-const Scene = () => {
-
+const Lights = () => {
   return (
     <>
       <ambientLight />
       <directionalLight
         position={[-5, 5, 5]}
         castShadow
-        shadow-mapSize={1024}
+        shadow-mapSize={2024}
       />
-
-      <RigidBody type="dynamic">
-        <Box castShadow position={[0, 5, 0]}>
-          <meshStandardMaterial color="darkslategrey" />
-        </Box>
-      </RigidBody>
-
-      <RigidBody type="fixed" name="floor" friction={0.75}>
-        <Box position={[0, 0, 0]} args={[100, 1, 100]} receiveShadow>
-          <meshStandardMaterial color="white" />
-        </Box>
-      </RigidBody>
     </>
+  );
+};
+
+
+
+const Scene = ({ children }:any) => {
+  return (
+    <div>
+      <Canvas shadows camera={{ fov: 50, position:[0,3,2] }}>
+      <color attach="background" args={["lightblue"]} />
+        <Lights />
+
+        <Physics gravity={[0, -9.8, 0]}>
+          {children}
+
+          <Floor receiveShadow rotation={[Math.PI / -2, 0, 0]} color="grey" />
+        </Physics>
+
+        <PointerLockControls />
+      </Canvas>
+    </div>
   );
 };
 
